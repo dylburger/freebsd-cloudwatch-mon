446728d71165fa174bc4851d248255db49cef46d
Dylan Sather <dylan.sather@gmail.com>
2016-10-31 05:56:59 +0000

    Fixing class method

--- MetricsMonitor.py
+++ MetricsMonitor.py
@@ -10,11 +10,11 @@ class CloudWatchClient(object):
         to CloudWatch
     """
     # Our config is shared across all Metrics
-    cfg = config.YAMLParser(config.CONFIG_FILE)
+    cfg = config.YAMLParser(config.CONFIG_FILE).config
 
     @classmethod
     def get_namespace(cls):
-        return cls.cfg.cloudwatch_namespace
+        return cls.cfg['cloudwatch_namespace']
 
     def __init__(self):
         self.client = boto3.client('cloudwatch')
