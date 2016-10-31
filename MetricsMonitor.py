59a4366a31cca06b308cb1bf92e51088daf7496a
Dylan Sather <dylan.sather@gmail.com>
2016-10-30 22:46:04 -0700

    Adding class method to get namespace

--- MetricsMonitor.py
+++ MetricsMonitor.py
@@ -10,11 +10,15 @@ class CloudWatchClient(object):
         to CloudWatch
     """
     # Our config is shared across all Metrics
-    config = config.YAMLParser(config.CONFIG_FILE)
+    cfg = config.YAMLParser(config.CONFIG_FILE)
+
+    @classmethod
+    def get_namespace(cls):
+        return cls.cfg.cloudwatch_namespace
 
     def __init__(self):
         self.client = boto3.client('cloudwatch')
-        self.namespace = config.cloudwatch_namespace
+        self.namespace = self.get_namespace()
 
     def put_metric_data(self, metric_data):
         self.client.put_metric_data(Namespace=self.namespace, MetricData=metric_data)
